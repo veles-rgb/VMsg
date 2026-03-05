1aef8f24b9d523bed4af2f88315c4dc470751b6f
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-03-05 15:11:34 -0500

    Separate routing logic using AuthContext

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,33 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useAuth } from './auth/AuthContext';
 import './App.css';
 
 // Pages
+import Welcome from './pages/Welcome.jsx';
 import Home from './pages/Home.jsx';
+import Register from './pages/Register.jsx';
+import Login from './pages/Login.jsx';
 
 function App() {
+  const { user } = useAuth();
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        {!user && (
+          <>
+            <Route path="/welcome" element={<Welcome />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </>
+        )}
+
+        {user && (
+          <>
+            <Route path="/" element={<Home />} />
+          </>
+        )}
+
+        <Route path="*" element={<Navigate to={user ? '/' : '/welcome'} />} />
       </Routes>
     </>
   );
