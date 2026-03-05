1c30293bf0109dfedc01246077537f0ee975baf2
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-03-05 15:07:47 -0500

    Create a verify route for frontend auth

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
+const { requireAuth } = require("../middleware/requireAuth");
 
 // Auth controller
 const controller = require('../controllers/auth.js');
 
+router.get('/verify', requireAuth, (req, res) => {
+    return res.json({ ok: true, user: req.user });
+});
 router.post('/register', controller.createUser);
 router.post("/login", controller.loginUser);
 
