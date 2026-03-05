f6c3033d8756da941f37b91a326155afd848a9f5
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-03-05 12:42:47 -0500

    Init client

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+
+// Pages
+import Home from './pages/Home.jsx';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
