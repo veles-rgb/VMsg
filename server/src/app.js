af476571dd26abff2e9194386a636942f6957637
MacMini <181009546+veles-rgb@users.noreply.github.com>
2026-03-30 15:11:30 -0400

    Fix CORS errors

--- server/src/app.js
+++ server/src/app.js
@@ -8,17 +8,30 @@ const app = express();
 const router = require("./routes/index");
 const { initSocket } = require("./socket");
 
-app.use(express.json());
-app.use(cors());
+// CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "https://vmsg.up.railway.app",
+        credentials: true,
+    })
+);
 
+app.options("*", cors());
+
+// Body parser
+app.use(express.json());
+
+// Routes
 app.use("/api", router);
 
 const PORT = process.env.SERVER_PORT || 3001;
 
 const server = http.createServer(app);
 
+// Initialize socket.io
 initSocket(server);
 
+// Start server
 server.listen(PORT, () => {
     console.log("Server is running on port", PORT);
 });
